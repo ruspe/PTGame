6942e0f7062c791a59cf47a50532afe09a6344ac
ruspe <cypekarek@gmail.com>
2023-04-19 22:06:20 -0400

    Variation in aiming that can be modified

--- PT Game/Assets/Scripts/moveAim.cs
+++ PT Game/Assets/Scripts/moveAim.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class moveAim : MonoBehaviour
 {
+    [Header("Horizontal")]
+    public float horizontalSpeed = 2f; //speed of aim moving on x axis
+    public float horizontalRange = 2f; //x axis aim range
+    private float minX;
+    private float maxX;
+
+    [Header("Vertical")]
+    public float verticalSpeed = 4f; //speed of aim moving on y axis
+    public float verticalRange = 2f; //y axis aim range
+    private float minY;
+    private float maxY;
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        minX = transform.position.x - horizontalRange;
+        maxX = transform.position.x + horizontalRange;
+        minY = transform.position.y - verticalRange;
+        maxY = transform.position.y + verticalRange;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(Mathf.PingPong(Time.time * horizontalSpeed, maxX - minX) + minX, Mathf.PingPong(Time.time * verticalSpeed, maxY-minY) + minY, transform.position.z);
+
     }
 }
