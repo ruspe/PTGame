94b271e565305ce1598e84f111f703852ae2dcaa
Jose Israel Montanez <jim46@drexel.edu>
2023-04-24 16:35:01 -0400

    Attempt #1 at updating catch script
    Assets\Scripts\catchProjectile.cs(44,67): error CS0839: Argument missing
    
    this error is only here until we add the fireball prefab to the game whenever the player is ready to throw the ball back.

--- PT Game/Assets/Scripts/catchProjectile.cs
+++ PT Game/Assets/Scripts/catchProjectile.cs
@@ -2,30 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class catchProjectile : MonoBehaviour
+public class CatchProjectile : MonoBehaviour
 {
+    public Transform respawnPoint; // Assign the respawn point for the fireball
 
-    //catches projectiles that collide with colliders that have this script
+    private bool isTrackingFireball; // Flag to track if a fireball is being tracked
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Set initial state to not tracking a fireball
+        isTrackingFireball = false;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.tag == "projectile")
+        if (collision.collider.CompareTag("projectile") && !isTrackingFireball)
         {
             Destroy(collision.gameObject);
             Debug.Log("Catch!");
+
+            // Call a method to track the fireball with UI indicator
+            TrackFireball();
+
+            // Set flag to track fireball
+            isTrackingFireball = true;
         }
+    }
+
+    private void TrackFireball()
+    {
+        // Implement logic to track the fireball with UI indicator
+        // This can involve showing a UI element, updating UI text, or any other visual indicator as needed
+
+        // Example: Show a UI indicator at a specific position on the screen
+        // Vector3 screenPosition = Camera.main.WorldToScreenPoint(respawnPoint.position);
+        // Instantiate(/* UI indicator prefab */, screenPosition, Quaternion.identity);
+
+        // Example: Update a UI text element with relevant information
+        // UI textElement = /* reference to the UI text element */;
+        // textElement.text = "Fireball caught!";
+    }
+
+    public void RespawnFireball()
+    {
+        // Instantiate a new fireball prefab at the respawn point
+        GameObject newFireball = Instantiate(/* Replace with your fireball prefab */, respawnPoint.position, Quaternion.identity);
+
+        // Set the fireball's velocity, direction, or any other properties as needed
+
+        // Reset the flag to stop tracking fireball
+        isTrackingFireball = false;
     }
 }
